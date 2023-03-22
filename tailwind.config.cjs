3f24a8c5745cafe3b91efec352d2fc2ba1b4aac0
DerianPaez <derianpaez01@hotmail.com>
2023-03-22 14:31:46 -0500

    Add header

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -1,12 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}"],
+  content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}', './node_modules/flowbite/**/*.js'],
   theme: {
     extend: {
       colors: {
-        primary: "#00AAFF",
+        gray: '#B5BCCB',
+        blue: '#0E1113'
       },
-    },
+      gradientColorStops: {
+        primary: '#00AAFF',
+        secondary: '#3C5A69'
+      },
+      gridTemplateColumns: {
+        header: 'max-content max-content 1fr max-content max-content'
+      }
+    }
   },
-  plugins: [],
-};
+  plugins: [require('flowbite/plugin')]
+}
