fe1e874f523dd0660114b1993af4ecee63851939
james.mendoza <james.mendoza@rappi.com>
2023-04-07 12:09:29 -0500

    Revert "Add vercel adapter"
    This reverts commit 2d0eac9a58edffc0dac6d918590f2a7365c40b7f.

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,10 @@
-import { defineConfig } from 'astro/config';
-import astroI18next from 'astro-i18next';
-import tailwind from '@astrojs/tailwind';
+import { defineConfig } from 'astro/config'
+import astroI18next from 'astro-i18next'
 
-import vercel from "@astrojs/vercel/serverless";
+import tailwind from '@astrojs/tailwind'
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), astroI18next()],
-  output: 'server',
-  adapter: vercel()
-});
+  output: 'server'
+})
--- src/pages/recaptcha.js
+++ src/pages/recaptcha.js
@@ -3,7 +3,7 @@ export async function post({ request }) {
 
   const recaptchaURL = 'https://www.google.com/recaptcha/api/siteverify'
   const requestBody = {
-    secret: '6Ld4zEYlAAAAALBmPdtTVtgLbgvoSBp2ujtzjfnu',
+    secret: '6Ld4zEYlAAAAALipx7cckd6dM74UdTBwWiwEw1hq',
     response: data.recaptcha
   }
 
